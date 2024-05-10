30b68feeedfe87f42c349d084a99f0f0ce768ec8
teodorazecevic8 <147929742+teodorazecevic8@users.noreply.github.com>
2024-05-10 16:01:22 +0200

    changed way of connecting
    add map db with connection file, and connection link is in .env

--- backend/app.js
+++ backend/app.js
@@ -1,59 +1,90 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors')
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// const cors = require('cors')
+// require("dotenv").config();
+// require('express-async-errors');
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/userRoutes');
+
+// var app = express();
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs')
+
+
+// // vključimo mongoose in ga povežemo z MongoDB
+// var mongoose = require('mongoose');
+// var mongoDB = "mongodb+srv://leafadmin:leaf123@leafy.gnw7mw8.mongodb.net/leafCollection";
+// mongoose.connect(mongoDB);
+// mongoose.Promise = global.Promise;
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/api/v1', usersRouter);
+// app.use(cors())
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
+
 require("dotenv").config();
 require('express-async-errors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/userRoutes');
+const connectDB = require("./db/connect");
+const express = require("express");
+const cors = require('cors')
+const app = express();
+const mainRouter = require("./routes/userRoutes");
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs')
-
-
-// vključimo mongoose in ga povežemo z MongoDB
-var mongoose = require('mongoose');
-var mongoDB = "mongodb+srv://leafadmin:leaf123@leafy.gnw7mw8.mongodb.net/leafCollection";
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/api/v1', usersRouter);
 app.use(cors())
+app.use("/api/v1", mainRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+const port = process.env.PORT || 3000;
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+const start = async () => {
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    try {        
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}`);
+        })
 
-module.exports = app;
+    } catch (error) {
+       console.log(error); 
+    }
+}
+
+start();
 
 
 
