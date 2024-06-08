1221de13e4695f3ef47be23c5a1f87d9182c3d0c
Nataša Đurić <natasa.djuric@student.um.si>
2024-06-08 17:13:02 +0200

    Adjusting the airQuality attributes

--- backend/controllers/airQualityController.js
+++ backend/controllers/airQualityController.js
@@ -98,16 +98,16 @@ module.exports = {
             }
 
             airQuality.station = req.body.station ? req.body.station : airQuality.station;
-            airQuality.timestamp = req.body.timestamp ? req.body.timestamp : airQuality.timestamp;
-            airQuality.so2 = req.body.so2 ? req.body.so2 : airQuality.so2;
-            airQuality.co = req.body.co ? req.body.co : airQuality.co;
             airQuality.pm10 = req.body.pm10 ? req.body.pm10 : airQuality.pm10;
             airQuality.pm25 = req.body.pm25 ? req.body.pm25 : airQuality.pm25;
+            airQuality.so2 = req.body.so2 ? req.body.so2 : airQuality.so2;
+            airQuality.co = req.body.co ? req.body.co : airQuality.co;
             airQuality.o3 = req.body.o3 ? req.body.o3 : airQuality.o3;
             airQuality.no2 = req.body.no2 ? req.body.no2 : airQuality.no2;
             airQuality.benzene = req.body.benzene ? req.body.benzene : airQuality.benzene;
             airQuality.isFake = req.body.isFake !== undefined ? req.body.isFake : airQuality.isFake;
-			
+            airQuality.timestamp = req.body.timestamp ? req.body.timestamp : airQuality.timestamp;
+
             airQuality.save(function (err, airQuality) {
                 if (err) {
                     return res.status(500).json({
@@ -127,7 +127,7 @@ module.exports = {
     remove: function (req, res) {
         var id = req.params.id;
 
-        AirqualityModel.findByIdAndRemove(id, function (err, airQuality) {
+        AirqualityModel.findByIdAndRemove(id, function (err) {
             if (err) {
                 return res.status(500).json({
                     message: 'Error when deleting the airQuality.',
